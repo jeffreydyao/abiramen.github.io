0e58e5c076241a09699fd0db10bc00f62757e9a4
abiramen <7523422+abiramen@users.noreply.github.com>
2021-01-24 02:29:56 +1100

    jQuery is now #cancelled

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,47 +1,28 @@
 var nav_hidden = false;
-var monochrome = false;
 var transparency = false;
 
-function setScrollHandler(bricks) {
-	$(window).scroll(brickHandler);
-	brickHandler();
-}
+const $ = document.querySelector.bind(document);
 
-function scrollCore() {
-	let scroll = $(this).scrollTop();
-	let text_top = $("#splash-text").offset().top;
-	var text_bottom = text_top + $("#splash-text").outerHeight(true);
-	if (nav_hidden && (scroll > text_bottom + 100 || scroll < text_top - 130)) {
-		$("#navbar").animate({
-			opacity: 1
-		}, 150);
-		nav_hidden = false;
-	} else if (!nav_hidden && scroll > text_top - 130 && scroll < text_bottom + 100) {
-		$("#navbar").animate({
-			opacity: 0
-		}, 150);
-		nav_hidden = true;
-	}
-	return scroll;
-}
+window.addEventListener("scroll", postHandler)
+postHandler();
 
-function brickHandler() {
-	let scroll = scrollCore();
-	
-	let brick_bottom = $("#post-splash").offset().top + $("#post-splash").outerHeight(true);
-	if (transparency && scroll > brick_bottom) {
-		toggleTransparencyOff();
-	} else if (!transparency && scroll < brick_bottom) {
-		toggleTransparencyOn();
-	}
-}
-
-function toggleTransparencyOff() {
-	$("nav").css("background", "#111");
-	transparency = false;
-}
-
-function toggleTransparencyOn() {
-	$("nav").css("background", "transparent");
-	transparency = true;
+function postHandler() {
+    let scroll = window.scrollY;
+    let text_top = $("#splash-text").offsetTop;
+    var text_bottom = text_top + $("#splash-text").offsetHeight;
+    if (nav_hidden && (scroll > text_bottom + 100 || scroll < text_top - 130)) {
+        $("#navbar").animate([{opacity: 1}], {duration: 150, fill: "forwards"});
+        nav_hidden = false;
+    } else if (!nav_hidden && scroll > text_top - 130 && scroll < text_bottom + 100) {
+        $("#navbar").animate([{opacity: 0}], {duration: 150, fill: "forwards"});
+        nav_hidden = true;
+    }
+    let brick_bottom = $("#post-splash").offsetTop + $("#post-splash").offsetHeight;
+    if (transparency && scroll > brick_bottom) {
+        $("nav").style.background = "#111";
+        transparency = false;
+    } else if (!transparency && scroll < brick_bottom) {
+        $("nav").style.background = "transparent";
+        transparency = true;
+    }
 }
