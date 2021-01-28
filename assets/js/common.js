759bf4ff0d7377058e9d5659f822cb9786f7bec3
abiramen <7523422+abiramen@users.noreply.github.com>
2021-01-29 03:19:43 +1100

    Add overlay

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,13 @@
 console.log("Hello there, fellow hackerperson. 🕵️‍♂️🕵️‍♀️")
 
+// Pranked, I'm not actually using jQuery.
+const $ = document.querySelector.bind(document);
+
 document.querySelectorAll('.page-anchor-offset').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
+        let nav_height = $('nav').offsetHeight + 5;
         e.preventDefault();
         var dest = document.querySelector(this.getAttribute('href'));
-        window.scroll(0, dest.offsetTop - 90);
+        window.scroll(0, dest.offsetTop - nav_height);
     });
 });
