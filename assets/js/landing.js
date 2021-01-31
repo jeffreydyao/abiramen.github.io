968e5cff387b67905d45dfb30b8062dd7b95366d
abiramen <7523422+abiramen@users.noreply.github.com>
2021-02-01 00:10:31 +1100

    Fix reflow when text overflows

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,23 +1,35 @@
-const line1 = '// i study computer science.';
-const line2 = '// also i really like ramen.';
+const line1 = '';
+const line2 = '';
 const base_speed = 40;
 
+
 let char_count = 0;
+let line_length = 0;
 
 const typewrite = () => {
+    
     let $ = document.querySelector.bind(document);
-    if (char_count < line1.length + line2.length) {
-        if (char_count < line1.length) {
-            $('#cursor-2').style.display = 'none';
-            $('#splash-line1').innerHTML += line1[char_count];
+    if (char_count === 0) {
+        $('#cursor-2').style.display = 'none';
+        line_length = $('#splash-line1-hidden').innerHTML.length;
+    }
+
+    if ($('#splash-line2-hidden').innerHTML.length != 0) {
+        if ($('#splash-line1-hidden').innerHTML.length != 0) {
+            $('#splash-line1').innerHTML += $('#splash-line1-hidden').innerHTML[0];
+            $('#splash-line1-hidden').innerHTML = $('#splash-line1-hidden').innerHTML.substring(1);
         } else {
+            $('#splash-line2').innerHTML += $('#splash-line2-hidden').innerHTML[0];
+            $('#splash-line2-hidden').innerHTML = $('#splash-line2-hidden').innerHTML.substring(1);
+        }
+        if (char_count === line_length) {
             $('#cursor-1').style.display = 'none';
             $('#cursor-2').style.display = 'inline-block';
-            $('#splash-line2').innerHTML += line2[char_count - line1.length];
         }
         char_count++;
-        setTimeout(typewrite, base_speed * (1 + Math.random()) + (char_count == line1.length) * 400);
+        setTimeout(typewrite, base_speed * (1 + Math.random()) + (char_count === line_length) * 400);
     }
 };
+
 
 typewrite();
